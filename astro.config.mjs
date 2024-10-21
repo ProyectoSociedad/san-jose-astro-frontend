69a9e115b22c1b26f8b303afa4728836b7a18558
SPBR <marketingsociedadperu@gmail.com>
2024-10-21 12:21:18 -0500

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-// import partytown from '@astrojs/partytown'
+import partytown from '@astrojs/partytown'
 import react from '@astrojs/react'
 
 // https://astro.build/config
