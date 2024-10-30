54763f7c21dc15a416a7ee9689571d5e018fd8ba
SPBR <marketingsociedadperu@gmail.com>
2024-10-30 16:36:52 -0500

    Terminado

--- src/assets/home/index.ts
+++ src/assets/home/index.ts
@@ -1,10 +1,14 @@
-import clasesPresenciales from '@/assets/home/clases-presenciales.png'
+import clasesPresenciales1 from '@/assets/home/clases-presenciales-1.png'
+import clasesPresenciales2 from '@/assets/home/clases-presenciales-2.png'
+import clasesPresenciales3 from '@/assets/home/clases-presenciales-3.png'
+import clasesPresenciales4 from '@/assets/home/clases-presenciales-4.png'
 import temario from '@/assets/home/temario.png'
 import joseCieza from '@/assets/home/jose-cieza.png'
 import luisHutchinson from '@/assets/home/luis-hutchinson.png'
 import renzoSeminario from '@/assets/home/renzo-seminario.png'
 import certificate from '@/assets/home/certificate.png'
 import ministerio from '@/assets/home/ministerio.png'
+import minedu from '@/assets/home/minedu.png'
 import contacto from '@/assets/home/contacto.png'
 import hero from '@/assets/home/hero.png'
 import diploma from '@/assets/home/diploma.png'
@@ -12,12 +16,16 @@ import diploma from '@/assets/home/diploma.png'
 export const homeImages = {
 	hero,
 	diploma,
-	clasesPresenciales,
+	clasesPresenciales1,
+	clasesPresenciales2,
+	clasesPresenciales3,
+	clasesPresenciales4,
 	temario,
 	joseCieza,
 	luisHutchinson,
 	renzoSeminario,
 	certificate,
+	minedu,
 	ministerio,
 	contacto
 }
