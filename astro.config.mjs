40b07d86166bf9b1f2afb36552d773e9a66c7bec
SPBR <marketingsociedadperu@gmail.com>
2025-01-06 11:34:12 -0500

    iPDAYE

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig, envField } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import react from '@astrojs/react'
-
+import { loadEnv } from 'vite'
+const { SITE } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://cursoagenteinmobiliarioperu.pe',
+	site: SITE,
 	integrations: [
 		// partytown({
 		// 	// Adds dataLayer.push as a forwarding-event.
@@ -13,12 +13,12 @@ export default defineConfig({
 		// 		forward: ['dataLayer.push']
 		// 	}
 		// })
-
 		tailwind(),
 		react()
 	],
 	env: {
 		schema: {
+			SITE: envField.string({ context: 'client', access: 'public' }),
 			API_INFORMATION: envField.string({ context: 'client', access: 'public' }),
 			PUBLIC_RECAPTCHA_SITE_KEY: envField.string({
 				context: 'client',
